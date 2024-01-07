c1431824420b92b809b74ff87e6cd15b66cda3f0
hann <hannmach711@gmail.com>
2024-01-07 17:57:08 +0000

    added temp search function to JS

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-function weatherSearchForm (event){
-event.preventDefault ();
-let searchElement=document.querySelector ("#search-city");
-let cityElement=document.querySelector ("#city");
-cityElement.innerHTML=searchElement.value;
-
-
+function refreshWeather (response){
+let temperatureElement=document.querySelector ("#temperature-unit");
+let temperature=response.data.temperature.current;
+temperatureElement.innerHTML=Math.round (temperature);
 }
 
 
+function citySearch (city){
+let apiKey="df33e33bd04657eta6o8f2efea2aa5fb";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+axios.get (apiUrl).then(refreshWeather);
 
+}
+
+function weatherSearchForm (event){
+event.preventDefault ();
+let searchElement=document.querySelector ("#search-city");
+let cityElement=document.querySelector ("#location");
+cityElement.innerHTML=searchElement.value;
+
+citySearch(searchElement.value);
+}
 
 let weatherForm=document.querySelector ("#search-form");
 weatherForm.addEventListener ("submit", weatherSearchForm);
+
