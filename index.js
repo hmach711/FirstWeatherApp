6fc048ce3a082f97281d225009677b0c1fb44782
hann <hannmach711@gmail.com>
2024-01-11 19:15:42 +0000

    added wind & humidity responses

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 function refreshWeather(response) {
-    let cityElement = document.querySelector("#city");
+    let cityElement = document.querySelector("#location");
     cityElement.innerHTML = response.data.city;
     let temperatureElement = document.querySelector(".weather-app-unit");
     let temperature = response.data.temperature.current;
+    let conditionElement=document.querySelector ("#condition");
+    let humidityElement=document.querySelector ("#humidity");
+    let windElement=document.querySelector ("#wind");
+
+
     temperatureElement.innerHTML = Math.round(temperature);
-  }
-  
+    conditionElement.innerHTML=response.data.condition.description;
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+  windElement.innerHTML=`${response.data.wind.speed}km/ph`;
+}
   function citySearch(city) {
     let apiKey = "df33e33bd04657eta6o8f2efea2aa5fb";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
